1275b1f57cf411bf71461d73a59f5a8b5de64354
Mohamed Hafidi <mohamed.hafidi.mh@gmail.com>
2026-04-07 23:01:01 +0200

    holiday-form Component angepasst

--- src/main/webui/src/app/holiday-form.component/holiday-form.component.ts
+++ src/main/webui/src/app/holiday-form.component/holiday-form.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HolidayDTO} from '../../../api';
 
 @Component({
   selector: 'app-holiday-form.component',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './holiday-form.component.html',
   styleUrl: './holiday-form.component.css',
 })
-export class HolidayFormComponent {
+export class HolidayFormComponent implements OnInit{
+  holidayForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.holidayForm = this.fb.group({
+      id: [null],
+      name: [''],
+      description: [''],
+      coverPath: [false]
+    });
+  }
+
+  onSubmit(): void {
+    const formValue: HolidayDTO = this.holidayForm.value;
+    console.log(formValue);
+  }
 }
