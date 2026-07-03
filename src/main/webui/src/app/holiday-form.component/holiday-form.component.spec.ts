2fe17b5497afb5f327cd390b87475322dd29586b
Mohamed Hafidi <mohamed.hafidi.mh@gmail.com>
2026-07-03 20:54:26 +0200

    Jest Configurated

--- src/main/webui/src/app/holiday-form.component/holiday-form.component.spec.ts
+++ src/main/webui/src/app/holiday-form.component/holiday-form.component.spec.ts
@@ -1,19 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HolidayFormComponent } from './holiday-form.component';
+import { HolidayResourceService } from '../../../api';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('HolidayFormComponent', () => {
   let component: HolidayFormComponent;
   let fixture: ComponentFixture<HolidayFormComponent>;
+  let holidayResourceService: {
+    apiHolidaysPost: jest.Mock;
+  };
+  let router = {
+    navigate: jest.fn()
+  };
 
   beforeEach(async () => {
+    holidayResourceService = {
+      apiHolidaysPost: jest.fn().mockReturnValue(of({}))
+    };
+
     await TestBed.configureTestingModule({
-      imports: [HolidayFormComponent]
+      imports: [HolidayFormComponent],
+      providers:[{
+        provide: HolidayResourceService,
+        useValue: holidayResourceService
+      },
+        {
+          provide: Router,
+          useValue: router
+        }]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HolidayFormComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
--- src/main/webui/src/app/holiday.component/holiday.component.spec.ts
+++ src/main/webui/src/app/holiday.component/holiday.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HolidayComponent } from './holiday.component';
+import {HolidayResourceService} from '../../../api';
+import {of} from 'rxjs';
 
 describe('HolidayComponent', () => {
   let component: HolidayComponent;
   let fixture: ComponentFixture<HolidayComponent>;
-
+  let holidayService: {
+    apiHolidaysGet: jest.Mock
+  }
   beforeEach(async () => {
+    holidayService = {
+      apiHolidaysGet: jest.fn().mockReturnValue(of([{}]))
+    }
     await TestBed.configureTestingModule({
-      imports: [HolidayComponent]
+      imports: [HolidayComponent],
+      providers: [{
+        provide: HolidayResourceService,
+        useValue: holidayService
+      }]
     })
     .compileComponents();
 
